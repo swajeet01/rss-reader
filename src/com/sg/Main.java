73302a5f21e7d3ff6cb839f44f575ba8d4176af5
swapfile3 <swajeetg768@gmail.com>
2021-07-11 19:14:17 +0530

    Initial commit ArgsParse initial version

---
+++ src/com/sg/Main.java
@@ -0,0 +1,9 @@
+package com.sg;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArgsParse.Args parsed = ArgsParse.parse(args);
+        // TODO: Create and execute thread
+    }
+}
